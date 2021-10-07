7b4e74b36cdf265cecebfb382523102f8a5ef48a
Christian Lück <christian@clue.engineering>
2021-10-07 08:42:38 +0200

    Skip sigchild check if `phpinfo()` has been disabled

--- src/Process.php
+++ src/Process.php
@@ -438,6 +438,10 @@ class Process extends EventEmitter
             return self::$sigchild;
         }
 
+        if (!\function_exists('phpinfo')) {
+            return self::$sigchild = false; // @codeCoverageIgnore
+        }
+
         \ob_start();
         \phpinfo(INFO_GENERAL);
 
